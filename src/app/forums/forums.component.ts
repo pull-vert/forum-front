e874d100c8493321c28a752b313bc862abca0278
pull-vert <frederic.montariol@gmail.com>
2017-01-30 23:56:13 +0100

    update angular-cli en beta-26 avec options --routing pour ajouter le Router (ajout fichiers oubliés)

---
+++ src/app/forums/forums.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ForumForumDoc } from '../data/documents/forum-forum-doc';
+
+import { ForumService } from '../services/forum.service';
+
+@Component({
+  selector: 'app-forums',
+  templateUrl: './forums.component.html',
+  styleUrls: ['./forums.component.css']
+})
+export class ForumsComponent implements OnInit {
+  forums: ForumForumDoc[];
+
+  constructor(
+    private router: Router,
+    private forumService: ForumService) { }
+
+  getForums(): void {
+    this.forumService.getForums().then(forums => this.forums = forums);
+  }
+
+  ngOnInit(): void {
+    this.getForums();
+  }
+
+  onSelect(forum: ForumForumDoc): void {
+    this.router.navigate(['/forum', forum._id]);
+  }
+}
