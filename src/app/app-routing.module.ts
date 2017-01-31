ebb2feb812b852eb9e646e72fa5be8114ebde289
pull-vert <frederic.montariol@gmail.com>
2017-02-01 00:52:10 +0100

    commit début routing OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ForumsComponent } from './forums/forums.component';
+
 const routes: Routes = [
   {
+    path: 'forums',
+    component: ForumsComponent
+  }/*,
+   {
+   path: 'login',
+   component: LoginComponent
+   },
+   {
+   path: 'courses',
+   component: CoursesComponent
+   }*/,
+  {
     path: '',
-    children: []
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
   }
 ];
 
