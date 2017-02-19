31b4609a8353ed6eeef636de801331f85e9b2a76
pull-vert <frederic.montariol@gmail.com>
2017-02-19 12:10:44 +0100

    commit migration angular-cli en 1.0.0-beta.32.3

--- protractor.conf.js
+++ protractor.conf.js
@@ -20,13 +20,13 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  useAllAngular2AppRoots: true,
   beforeLaunch: function() {
     require('ts-node').register({
       project: 'e2e'
     });
   },
-  onPrepare: function() {
-    jasmine.getEnv().addReporter(new SpecReporter());
+  onPrepare() {
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
+
